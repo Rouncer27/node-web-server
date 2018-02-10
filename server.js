c46a6ec4b52001b331cc98ed5580849f385e73b9
Trevor Rounce <trevorrounce@Trevors-iMac.hitronhub.home>
2018-02-10 10:50:21 -0700

    getting ready for heroku

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 
+const port = process.env.port || 3010;
+
 var app = express();
 
 hbs.registerPartials( __dirname + '/views/partials');
@@ -61,7 +63,7 @@ app.get( '/bad', ( req, res ) => {
     });
 });
 
-app.listen(3010, () => {
-    console.log('Server is up on port 3010');
+app.listen(port, () => {
+    console.log(`Server is up on port ${port}`);
 });
 
